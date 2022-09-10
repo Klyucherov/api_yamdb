a932ee37bbaa6b7fcc0f9a854cc8ef8a670e4c22
Klyucherov Evgeniy <Klyucherov.e@yandex.ru>
2022-09-11 01:07:41 +0300

    V4

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -4,22 +4,20 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsAdministratorRole(BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.user.is_authenticated
+        return (request.user.is_authenticated
                 and request.user.is_admin
                 or request.user.is_superuser
-        )
+                )
 
 
 class IsAdminOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
-        return (
-                request.method in SAFE_METHODS
+        return (request.method in SAFE_METHODS
                 or request.user.is_authenticated
                 and request.user.is_admin
                 or request.user.is_superuser
-        )
+                )
 
 
 class IsSuperuserAdminModeratorAuthorOrReadOnly(BasePermission):
@@ -32,12 +30,11 @@ class IsSuperuserAdminModeratorAuthorOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         safe_method = request.method in SAFE_METHODS
-        access = (
-                request.user == obj.author
-                or request.user.is_superuser
-                or request.user.is_authenticated
-                and request.user.is_admin
-                or request.user.is_authenticated
-                and request.user.is_moderator
-        )
+        access = (request.user == obj.author
+                  or request.user.is_superuser
+                  or request.user.is_authenticated
+                  and request.user.is_admin
+                  or request.user.is_authenticated
+                  and request.user.is_moderator
+                  )
         return safe_method or access
