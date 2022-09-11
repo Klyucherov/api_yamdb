63cc541ee365c35ce2c5266860c704a0063efce7
doomkirov <doomnewera@gmail.com>
2022-09-11 07:59:21 +0300

    Some changes to requirements.txt

--- api_yamdb/reviews/management/commands/load_data.py
+++ api_yamdb/reviews/management/commands/load_data.py
@@ -1,37 +1,34 @@
-import csv
+from csv import DictReader
 
 from django.core.management import BaseCommand
+
 from reviews.models import (
     User, Title, Category, Genre, GenreTitle, Review, Comment
 )
 
-CSV_FILES = [
-    ['users.csv', User],
-    ['category.csv', Category],
-    ['genre.csv', Genre],
-    ['titles.csv', Title],
-    ['genre_title.csv', GenreTitle],
-    ['review.csv', Review],
-    ['comments.csv', Comment],
+
+CSV_MODELS = [
+    [User, 'users.csv'],
+    [Category, 'category.csv'],
+    [Genre, 'genre.csv'],
+    [Title, 'titles.csv'],
+    [GenreTitle, 'genre_title.csv'],
+    [Review, 'review.csv'],
+    [Comment, 'comments.csv'],
 ]
 
-
 class Command(BaseCommand):
-    help = 'import csv data into database'
+    help = "Loads data from static/data/*.csv files"
 
     def handle(self, *args, **options):
-
-        self.stdout.write(self.style.NOTICE('Очистка базы данных'))
-
-        for _, model in reversed(CSV_FILES):
+        self.stdout.write('Удаляем старые данные...')
+        for model, file in CSV_MODELS:
             model.objects.all().delete()
+        self.stdout.write(self.style.NOTICE('Заполняем базу данных...'))
 
-        self.stdout.write(self.style.NOTICE('Подготовка новых данных'))
-
-        for file, model in CSV_FILES:
-            with open('static/data/' + file, 'r', encoding='utf-8') as f:
-                reader = csv.DictReader(f)
-                for dict_row in reader:
-                    model.objects.get_or_create(**dict_row)
-
-        self.stdout.write(self.style.SUCCESS('Новые данные внесены в базу'))
+        for model, file in CSV_MODELS:
+            for row in DictReader(
+                open('static/data/'+ file, mode='r', encoding='utf-8')
+            ):
+                model.objects.get_or_create(**row)
+        self.stdout.write(self.style.SUCCESS('Успешно!'))
